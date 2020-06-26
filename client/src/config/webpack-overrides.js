c83a1b76a4a3f37fffbcf8e61470aa0b7aa96bc8
Cezar Berea <berea.cezar@gmail.com>
2020-06-26 11:35:33 +0300

    ETV-1383 Server i18n dictionaries from app server

--- client/src/config/webpack-overrides.js
+++ client/src/config/webpack-overrides.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { paths } = require('react-app-rewired')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   webpack: function(config) {
@@ -10,6 +11,18 @@ module.exports = {
       '@assets': path.resolve(paths.appPath, `${paths.appSrc}/assets`),
       app: path.resolve(paths.appPath, `${paths.appSrc}/app`)
     }
+
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'src/shared/i18n/dictionaries/*.json',
+            to: 'static/i18n',
+            flatten: true
+          }
+        ]
+      })
+    )
     return config
   }
 }
--- client/src/shared/i18n/index.js
+++ client/src/shared/i18n/index.js
@@ -2,14 +2,10 @@ import { I18nService } from '@etvas/i18n'
 import languages from './languages'
 import en from './dictionaries/en'
 
-const url =
-  process.env.REACT_APP_PUBLIC_S3_URL ||
-  'https://etvas-public-assets-dev.s3.eu-central-1.amazonaws.com'
-
 export const i18nService = new I18nService(languages, {
   defaultLanguage: 'en',
   sessionKey: 'etvas.language',
-  dictionaryUrl: `${url}/i18n/hello-etvas/{lang}.json`
+  dictionaryUrl: '/static/i18n/{lang}.json'
 })
 
 i18nService.loadDictionary('en', en)
