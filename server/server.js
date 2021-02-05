bb9e078f587715cb8549b1e8f1893662ddf07343
Onu Radu Bogdan <bogdan.onu@thinslices.com>
2021-02-05 10:46:36 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const etvas = require('@etvas/etvas-sdk')
 etvas.init({
   apiKey: process.env.ETVAS_API_KEY,
   apiURL: process.env.ETVAS_REST_URI,
-  eventSecret: process.env.ETVAS_EVENT_SECRET,
+  eventSecret: process.env.ETVAS_EVENT_SECRET || 'DEV_PLACEHOLDER',
 })
 
 const app = express()
