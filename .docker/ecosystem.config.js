292c66328fc1ad2735e332b36c6cea72e122c57d
Sergiu Elisei <sergiu.elisei@thinslices.com>
2021-03-24 14:22:11 +0200

    feat(pm2-logs) add logs

--- .docker/ecosystem.config.js
+++ .docker/ecosystem.config.js
@@ -5,12 +5,15 @@ module.exports = {
       cwd: 'server',
       script: 'server.js',
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
       },
       env_production: {
         PORT: 5000,
-        NODE_ENV: 'production'
-      }
-    }
-  ]
+        NODE_ENV: 'production',
+      },
+      error_file: '/var/log/pm2/server-error.log',
+      out_file: '/var/log/pm2/server-out.log',
+      combine_logs: true,
+    },
+  ],
 }
